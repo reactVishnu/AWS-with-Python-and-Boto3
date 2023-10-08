b07ec3fd7fbf2f23a3b3882d07a57237b6ecb745
reactVishnu <thirdparties47@gmail.com>
2023-10-08 21:36:43 +0530

    s3 client completed

--- s3/client_boto3.py
+++ s3/client_boto3.py
@@ -1,4 +1,10 @@
 import boto3 as aws
 
 s3 = aws.client('s3')
-s3.create_bucket(Bucket='my-first-bucket') # Gonna throw error if name is not unique
+# s3.create_bucket(Bucket='dockvulner-my-first-bucket') # Gonna throw error if name is not unique
+
+# with open('my-bucket.txt', 'w') as file:
+#     file.write('My bucket upload')
+
+# s3.upload_file(Filename='my-bucket.txt', Bucket='dockvulner-my-first-bucket', Key='first_file')
+# s3.download_file(Bucket='dockvulner-my-first-bucket', Key='first_file', Filename='downloaded_file')
